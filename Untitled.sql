e0826696123dc318c4943ba4c7af992147da42ee
Shawn Fielding <shawnfielding@thePro.local>
2017-05-02 18:18:42 -0400

    it doesn't work

--- Untitled.sql
+++ Untitled.sql
@@ -17,24 +17,7 @@ CREATE Table (
 item_id INTEGER(10) NOT NULL AUTO_INCREMENT,
 production_name VARCHAR(100),
 department_name VARCHAR(100),
-price Decimal 
+price Decimal
 
 
-
-
-
-
-
-CREATE TABLE top5000 (
-  id INT NOT NULL AUTO_INCREMENT,
-  position INT NOT NULL,
-  artist VARCHAR(100) NULL,
-  song VARCHAR(100) NULL,
-  songYear INT(5) NULL,
-  rawTotal DECIMAL(10,4) NULL,
-  rawUS DECIMAL(10,4) NULL,
-  rawUK DECIMAL(10,4) NULL,
-  rawEur DECIMAL(10,4) NULL,
-  rawRow DECIMAL(10,4) NULL,
-  PRIMARY KEY (id)
 )
